0ed8dbbb992760102716d5bd96735d395b446977
Hafshy <hafshy@gmail.com>
2023-10-30 21:16:11 +0700

    feat: add env

--- main.go
+++ main.go
@@ -5,13 +5,24 @@ import (
 
 	"github.com/hafshy/students-super-app/configs"
 	"github.com/hafshy/students-super-app/routes"
+	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
+	loadEnv()
 	configs.InitDB()
 	e := echo.New()
 	routes.InitRoute(e)
 	e.Start(":8080")
 	fmt.Println("Hello World!")
+}
+
+func loadEnv() {
+	err := godotenv.Load()
+	if err!= nil {
+        panic("Error loading.env file")
+    }
+
+
 }
